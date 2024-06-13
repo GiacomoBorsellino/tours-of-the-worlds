b5139730e89d09008c8fb9d8a6242c12508aff0a
GiacomoBorsellino <giacomoborsellino@hotmail.it>
2024-06-13 21:01:08 +0200

    fix search

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Card from "./components/Card/Card";
 const App = () => {
   const [tours, setTours] = useState([]);
   const [searchedTour, setSearchedTour] = useState('');
-
+  
   const getTours = async () => {
     setTours(cities);
   };
@@ -15,36 +15,38 @@ const App = () => {
     getTours();
   }, []);
 
-  function search() {
-
+  const search = (event) => {
+    setSearchedTour(event.target.value);
   }
+
+  const filteredTours = tours.filter(tour => 
+    tour.title.toLowerCase().includes(searchedTour.toLowerCase())
+  );
 
   return (
     <>
       <h1 className="text-center font-semibold text-4xl m-8 text-white">Tours of the World</h1>
-      {/* <form class="w-full max-w-sm mx-auto bg-white rounded-md">
-  <div class="flex items-center border-b border-gray-500 py-2">
-    <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Jane Doe" aria-label="Full name"/>
-    <button onClick={search} className="flex-shrink-0 bg-gray-500 hover:bg-gray-700 border-gray-500 hover:border-gray-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
-      Search
-    </button>
-    <button onClick={reset} className="flex-shrink-0 border-transparent border-4 text-gray-500 hover:text-gray-800 text-sm py-1 px-2 rounded" type="button">
-      Reset
-    </button>
-  </div>
-</form> */}
+      <div className="w-1/6 flex items-center border-b border-gray-500 py-2 mx-auto bg-white rounded-md m-4">
+        <input 
+          onChange={search} 
+          className="appearance-none bg-transparent border-none text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" 
+          type="text" 
+          placeholder="Search your tour" 
+          aria-label="tour"
+        />
+      </div>
 
       <section className="flex flex-row flex-wrap justify-center items-center gap-6 w-4/5 mx-auto">
         {
-          tours.map((tour) => (
+          filteredTours.map(tour => (
             <Card
               key={tour.id}
               title={tour.title}
               img={tour.img}
               description={tour.info}
-              isVisited={tour.isVisited}></Card>
+              isVisited={tour.isVisited}
+            />
           ))
-          // const { id, name, image, info, price } = tour;
         }
       </section>
     </>
